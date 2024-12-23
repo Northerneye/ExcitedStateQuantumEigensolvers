561d738bc751628792371e911c6d71fb6e738d2e
Northerneye <cameron.cianci@gmail.com>
2024-12-23 14:00:58 -0500

    Made Ground State Eigensolvers Callable

--- Ground State Eigensolvers/ExampleQITE.py
+++ Ground State Eigensolvers/ExampleQITE.py
@@ -1,20 +1,27 @@
 import numpy as np
 import qiskit
-from qiskit_algorithms import TimeEvolutionProblem, VarQITE
 from qiskit.quantum_info import SparsePauliOp
-from qiskit_algorithms.time_evolvers.variational import ImaginaryMcLachlanPrinciple
 
-var_principle = ImaginaryMcLachlanPrinciple()
-estimator = qiskit.primitives.Estimator(options={"shots": 1024})
+def VarQITE(H, total_time=5.0):
+    from qiskit_algorithms import TimeEvolutionProblem, VarQITE
+    from qiskit_algorithms.time_evolvers.variational import ImaginaryMcLachlanPrinciple
+    
+    var_principle = ImaginaryMcLachlanPrinciple()
+    estimator = qiskit.primitives.Estimator(options={"shots": 1024})
+
+    # Append the tunable parameters
+    ansatz = qiskit.circuit.library.TwoLocal(2, ["rx", "ry"], "cz", reps=1)
+    params = np.random.random(ansatz.num_parameters)
+
+    # Define and Run the Time Evolution Problem
+    evolution_problem = TimeEvolutionProblem(H, total_time, aux_operators=[H])
+    qrte = VarQITE(ansatz, params[:], var_principle, estimator, num_timesteps=50)
+    result = qrte.evolve(evolution_problem).observables[-1][0][0]
+    
+    return result
+
+
 H = SparsePauliOp.from_list([("II", -1.5), ("IZ", 0.5), ("ZI", -0.5), ("XX", 1.5),])
 
-# Append the tunable parameters
-ansatz = qiskit.circuit.library.TwoLocal(2, ["rx", "ry"], "cz", reps=1)
-params = np.random.random(ansatz.num_parameters)
-
-# Define and Run the Time Evolution Problem
-total_time = 5.0
-evolution_problem = TimeEvolutionProblem(H, total_time, aux_operators=[H])
-qrte = VarQITE(ansatz, params[:], var_principle, estimator, num_timesteps=50)
-result = qrte.evolve(evolution_problem).observables[-1][0][0]
-print(result)
+energy = QITE(H)
+print("Energy: "+str(energy))
--- Ground State Eigensolvers/ExampleVQE.py
+++ Ground State Eigensolvers/ExampleVQE.py
@@ -3,15 +3,20 @@ import qiskit
 import qiskit_algorithms
 from qiskit.quantum_info import SparsePauliOp
 
-H = SparsePauliOp.from_list([("II", -1.5), ("IZ", 0.5), ("ZI", -0.5), ("XX", 1.5),])
-estimator = qiskit.primitives.Estimator(options={"shots": 1024})
-optimizer = qiskit_algorithms.optimizers.COBYLA()
+def VQE(H, shots=1024):
+    estimator = qiskit.primitives.Estimator(options={"shots": shots})
+    optimizer = qiskit_algorithms.optimizers.COBYLA()
 
-# Define the ansatz which will be used to prepare the ground state
-ansatz = qiskit.circuit.library.TwoLocal(2, ["rx", "ry"], "cz", reps=1)
-initial_params = np.random.random(ansatz.num_parameters)
+    # Define the ansatz which will be used to prepare the ground state
+    ansatz = qiskit.circuit.library.TwoLocal(2, ["rx", "ry"], "cz", reps=1)
+    
+    # Run VQE and get results
+    vqe = qiskit_algorithms.VQE(estimator, ansatz, optimizer)
+    result = vqe.compute_minimum_eigenvalue(operator=H)
+    return result.eigenvalue
 
-# Run VQE and get results
-vqe = qiskit_algorithms.VQE(estimator, ansatz, optimizer)
-result = vqe.compute_minimum_eigenvalue(operator=H)
-print("Energy: "+str(result.eigenvalue))
+if __name__ == "__main__":
+    H = SparsePauliOp.from_list([("II", -1.5), ("IZ", 0.5), ("ZI", -0.5), ("XX", 1.5),])
+
+    energy = VQE(H)
+    print("Energy: "+str(energy))
