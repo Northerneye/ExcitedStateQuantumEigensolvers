ffc0249f7d2eb5ee5812615955a1f726c3239a79
Northerneye <cameron.cianci@gmail.com>
2025-03-22 18:03:59 -0400

    Shortened qEOM

--- Excited State Algorithms/qEOM.py
+++ Excited State Algorithms/qEOM.py
@@ -13,11 +13,8 @@ from qiskit_nature.second_q.algorithms import GroundStateEigensolver, QEOM
 
 # Set up H2 problem using PySCF
 molecule = MoleculeInfo(
-    # Coordinates in Angstrom
-    symbols=["H", "H"],
     coords=([0.0, 0.0, 0.0], [1.0, 0.0, 0.0]),
-    multiplicity=1,  # = 2*spin + 1
-    charge=0,
+    multiplicity=1, charge=0, symbols=["H", "H"],
 )
 driver = PySCFDriver.from_molecule(molecule)
 properties = driver.run()
@@ -25,16 +22,13 @@ transformer = FreezeCoreTransformer()
 problem = transformer.transform(properties)
 mapper=ParityMapper(num_particles=problem.num_particles)
 
-
 # Create the ansatz quantum circuit which will be used
 ansatz = TwoLocal(problem.num_spatial_orbitals, rotation_blocks=["rx", "ry"], entanglement_blocks="cz", entanglement="linear", reps=2)
 
 # Solve the ground state of the system with VQE
-estimator = Estimator()
-vqe = VQE(estimator, ansatz, COBYLA())
+vqe = VQE(Estimator(), ansatz, COBYLA())
 gse = GroundStateEigensolver(mapper, vqe)
 
-# Run qEOM on the VQE Ground state found
-qeom_excited_states_solver = QEOM(gse, estimator, excitations="sd")
-energies = qeom_excited_states_solver.solve(problem).eigenvalues
-print(energies)
+# Run qEOM on the VQE states found
+qeom_excited_states_solver = QEOM(gse, Estimator(), excitations="sd")
+print(qeom_excited_states_solver.solve(problem).eigenvalues)
